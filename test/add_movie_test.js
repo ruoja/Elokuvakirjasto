d620eaf23c9f483ca757558174164170ab50543e
ruoja <ruoja74@gmail.com>
2015-05-01 21:01:09 +0300

    elokuvien listaaminen ja lisääminen

--- test/add_movie_test.js
+++ test/add_movie_test.js
@@ -1,53 +1,85 @@
-describe('Add movie', function(){
-	var controller, scope;
+describe('Add movie', function () {
+    var controller, scope;
 
-	var FirebaseServiceMock;
+    var FirebaseServiceMock;
 
-  	beforeEach(function(){
-  		// Lisää moduulisi nimi tähän
-    	module('MyAwesomeModule');
+    beforeEach(function () {
+        // Lisää moduulisi nimi tähän
+        module('MovieApp');
 
-    	FirebaseServiceMock = (function(){
-			return {
-				// Toteuta FirebaseServicen mockatut metodit tähän
-			}
-		})();
+        FirebaseServiceMock = (function () {
+            var movies = [
+                {
+                    name: 'leffa',
+                    director: 'spielberg',
+                    year: 1980,
+                    description: 'awesome'
+                },
+                {
+                    name: 'toinenLeffa',
+                    director: 'tarantino',
+                    year: 2000,
+                    description: 'great'
+                }
+            ];
 
-		// Lisää vakoilijat
-	    // spyOn(FirebaseServiceMock, 'jokuFunktio').and.callThrough();
+            return {
+                addMovie: function (movie) {
+                    movies.push(movie);
+                },
+                getMovies: function () {
+                    return movies;
+                }
+            };
+        })();
 
-    	// Injektoi toteuttamasi kontrolleri tähän
-	    inject(function($controller, $rootScope) {
-	      scope = $rootScope.$new();
-	      // Muista vaihtaa oikea kontrollerin nimi!
-	      controller = $controller('MyAwesomeController', {
-	        $scope: scope,
-	        FirebaseService: FirebaseServiceMock
-	      });
-	    });
-  	});
+        // Lisää vakoilijat
+        spyOn(FirebaseServiceMock, 'getMovies').and.callThrough();
+        spyOn(FirebaseServiceMock, 'addMovie').and.callThrough();
 
-  	/*
-  	* Testaa alla esitettyjä toimintoja kontrollerissasi
-  	*/
+        // Injektoi toteuttamasi kontrolleri tähän
+        inject(function ($controller, $rootScope) {
+            scope = $rootScope.$new();
+            // Muista vaihtaa oikea kontrollerin nimi!
+            controller = $controller('AddController', {
+                $scope: scope,
+                FirebaseService: FirebaseServiceMock
+            });
+        });
+    });
 
-  	/*
-  	* Testaa, että käyttäjä pystyy lisäämään elokuvan oikeilla tiedoilla.
-  	* Muista myös tarkistaa, että Firebasen kanssa keskustelevasta palvelusta
-  	* on kutsutta oikeaa funktiota lisäämällä siihen vakoilijan ja käyttämällä
-  	* toBeCalled-oletusta.
-	*/
-	it('should be able to add a movie by its name, director, release date and description', function(){
-		expect(true).toBe(false);
-	});
+    /*
+     * Testaa alla esitettyjä toimintoja kontrollerissasi
+     */
 
-	/*	
-	* Testaa, ettei käyttäjä pysty lisäämään elokuvaa väärillä tiedoilla.
-	* Muista myös tarkistaa, että Firebasen kanssa keskustelevasta palvelusta
-	* EI kutsuta funktiota, joka hoitaa muokkauksen. Voit käyttää siihen
-	* not.toBeCalled-oletusta (muista not-negaatio!).
-	*/
-	it('should not be able to add a movie if its name, director, release date or description is empty', function(){
-		expect(true).toBe(false);
-	});
+    /*
+     * Testaa, että käyttäjä pystyy lisäämään elokuvan oikeilla tiedoilla.
+     * Muista myös tarkistaa, että Firebasen kanssa keskustelevasta palvelusta
+     * on kutsutta oikeaa funktiota lisäämällä siihen vakoilijan ja käyttämällä
+     * toBeCalled-oletusta.
+     */
+    it('should be able to add a movie by its name, director, release date and description', function () {
+        scope.name = 'kolmasLeffa';
+        scope.director = 'spielberg';
+        scope.year = 1990;
+        scope.description = 'sex & violence';
+        scope.addMovie();
+        expect(scope.movies[2].name).toBe('kolmasLeffa');
+        expect(FirebaseServiceMock.addMovie).toHaveBeenCalled();
+    });
+
+    /*	
+     * Testaa, ettei käyttäjä pysty lisäämään elokuvaa väärillä tiedoilla.
+     * Muista myös tarkistaa, että Firebasen kanssa keskustelevasta palvelusta
+     * EI kutsuta funktiota, joka hoitaa muokkauksen. Voit käyttää siihen
+     * not.toBeCalled-oletusta (muista not-negaatio!).
+     */
+    it('should not be able to add a movie if its name, director, release date or description is empty', function () {
+        scope.name = 'kolmasLeffa';
+        scope.director = '';
+        scope.year = 1990;
+        scope.description = 'sex & violence';
+        scope.addMovie();
+        expect(scope.movies.length).toBe(2);
+    });
 });
--- web/app/app.js
+++ web/app/app.js
@@ -1,4 +1,4 @@
-var MovieApp = angular.module('MovieApp', ['firebase']);
+var MovieApp = angular.module('MovieApp', ['firebase', 'ngRoute']);
 
 MovieApp.service('FirebaseService', function ($firebase) {
     var firebaseRef = new Firebase('https://sweltering-fire-6863.firebaseio.com/movies');
@@ -14,17 +14,40 @@ MovieApp.service('FirebaseService', function ($firebase) {
     };
 });
 
+MovieApp.config(function ($routeProvider) {
+    $routeProvider
+            .when('/', {
+                controller: 'ListController',
+                templateUrl: 'movies.html'
+            })
+            .when('/movies', {
+                controller: 'ListController',
+                templateUrl: 'movies.html'
+            })
+            .when('/movies/new', {
+                controller: 'AddController',
+                templateUrl: 'new.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+});
+
 MovieApp.controller('ListController', function ($scope, FirebaseService) {
     $scope.movies = FirebaseService.getMovies();
 });
 
-MovieApp.controller('AddController', function ($scope, FirebaseService) {
-    $scope.addMovie = function(movie) {
-        FirebaseService.addMovie({
-            name: $scope.name,
-            director: $scope.director,
-            year: $scope.year,
-            description: $scope.description
-        });
+MovieApp.controller('AddController', function ($scope, FirebaseService, $location) {
+    $scope.movies = FirebaseService.getMovies();
+    $scope.addMovie = function (movie) {
+        if ($scope.name !== '' && $scope.director !== '' && $scope.year !== '' && $scope.description !== '') {
+            FirebaseService.addMovie({
+                name: $scope.name,
+                director: $scope.director,
+                year: $scope.year,
+                description: $scope.description
+            });
+            $location.path('/movies');
+        }
     };
 });
